a1f70d7e70675555fb03b319d3e5345c31bdf9b4
dznomid <dznomid@gmail.com>
2023-04-09 20:47:44 +0200

    chess_with_horse_self

--- src/Horse.java
+++ src/Horse.java
@@ -9,11 +9,24 @@ public class Horse extends ChessPiece{
     }
 
     public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn){
-        if (checkPos(toLine) && checkPos(toColumn)) {
-            if (((toLine == line + 1)||(toLine == line - 1)) && ((toColumn == toColumn + 2)||(toColumn == column - 2))){
-                return true;
-            } else return false;
+        if (checkPos(toLine) && checkPos(toColumn) && checkPos(line) && checkPos(column)) {
+            if (line != toLine && column != toColumn
+                    && (chessBoard.board[toLine][toColumn] == null || !chessBoard.board[toLine][toColumn].color.equals(this.color))
+                    && chessBoard.board[line][column] != null){
+                int [][] positions = new int[][]{
+                        {line + 1, column + 2}, {line + 1, column - 2},
+                        {line -1, column + 2}, {line - 1, column -2},
+                        {line + 2, column + 1}, {line + 2, column - 1},
+                        {line - 2, column + 1}, {line - 2, column - 1}
+                };
+                for (int i = 0; i < positions.length; i++){
+                    if (positions[i][0] == toLine && positions[i][1] == toColumn){
+                        return true;
+                    }
+                }
+            }
         }else return false;
+        return false;   //!!!!!!!!!! maybe true...
     }
     public String getSymbol(){
         return("H");
